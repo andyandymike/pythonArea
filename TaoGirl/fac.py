cb2e11da5387816c8cffb0096b6fd3fc5c307ca9
andyandymike <564458508@qq.com>
2017-03-22 17:39:07 +0800

    work update

--- TaoGirl/fac.py
+++ TaoGirl/fac.py
@@ -1,10 +1,22 @@
+import os
 
 
-file = open('C:\\Users\\i067382\\Documents\\DS\\HANA CalView\\table_names.sql', 'r')
-target = open('C:\\Users\\i067382\\Documents\\DS\\HANA CalView\\convert.sql', 'a')
-tableNames = file.readlines()
-for tableName in tableNames:
-    #target.write('select * from ' + tableName + ' into ' + tableName + '_T;\n')
-    #target.write('drop table ' + tableName + ';\n')
-    #target.write('rename table ' + tableName + '_T to ' + tableName + ';\n\n')
-    target.write('drop table ' + tableName + '_T;\n')
+target = 'C:\\Users\\i067382\\Documents\\DS\\cassandra\\temp.txt'
+if os.path.isfile(target):
+    os.remove(target)
+targetFile = open('C:\\Users\\i067382\\Documents\\DS\\cassandra\\temp.txt', 'a', encoding='utf-8')
+
+for i in range(1, 37):
+    if i < 10:
+        targetFile.write('!testcase tcase'+str(i)+' odbc00'+str(i)+'.atl\n')
+        targetFile.write('!sh eim_launcher.sh odbc00' + str(i) + '\n')
+    else:
+        targetFile.write('!testcase tcase' + str(i) + ' odbc0' + str(i) + '.atl\n')
+        targetFile.write('!sh eim_launcher.sh odbc0' + str(i) + '\n')
+    targetFile.write('!expect no *Failed*\n')
+    if i < 10:
+        targetFile.write('!sh adiff ${runtest}/goldlog/odbc00'+str(i)+'.out ${runtest}/work/odbc00'+str(i)+'.out\n')
+    else:
+        targetFile.write('!sh adiff ${runtest}/goldlog/odbc0' + str(i) + '.out ${runtest}/work/odbc0' + str(i) + '.out\n')
+    targetFile.write('!endtestcase\n')
+    targetFile.write('\n')
